c1a11cbb2a0f61d2aa476433e32a61e7bf45d601
sara-daw <sriverocaminero.guadalupe@alumnado.fundacionloyola.net>
2026-02-13 12:52:26 +0100

    Add files via upload

---
+++ mostrar_datos POST.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ponentes Charlas</title>
+    <!--<link rel="stylesheet" href="./css.css">-->
+</head>
+<body>
+    <?php
+    echo "<h2>Hola</h2>";
+
+    $algo = "probar formulario";
+    echo "<p>$algo</p>";
+
+    echo "<h2>DATOS DEL PONENTE</h2>";
+    /* todas las variables empiezan por $_ */
+    /* el . en php concatena */
+    /* no se crean variables independientes */
+    /* Una variable dentro de comillas simples ('') siempre tiene que concatenar, con las dobles ("") no tiene porque */
+    echo '<p>'.$_POST["nombre"].'</p>';
+    echo '<p>'.$_POST["info"].'</p>';
+    echo '<p>'.$_POST["correo"].'</p>';
+    echo '<p>'.$_POST["tema"].'</p>';
+    ?>
+</body>
+</html>
